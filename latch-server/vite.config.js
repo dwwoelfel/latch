cf59ff2e6c43078b33c41d650169f5c1b22d54d6
Daniel Woelfel <dwwoelfel@gmail.com>
2023-06-05 21:52:18 -0700

    sourcemaps

--- latch-server/vite.config.js
+++ latch-server/vite.config.js
@@ -2,6 +2,9 @@ import {splitVendorChunkPlugin, defineConfig} from 'vite';
 import react from '@vitejs/plugin-react';
 
 export default defineConfig({
+  build: {
+    sourcemap: true,
+  },
   plugins: [
     splitVendorChunkPlugin(),
     react({
