6793c68558151e4d724dc400d28b18484cd1d89d
Daniel Woelfel <dwwoelfel@gmail.com>
2023-06-05 22:46:36 -0700

    the relay plugin looks at relay config

--- latch-server/vite.config.js
+++ latch-server/vite.config.js
@@ -4,13 +4,12 @@ import react from '@vitejs/plugin-react';
 export default defineConfig({
   build: {
     sourcemap: true,
-    minify: false,
   },
   plugins: [
     splitVendorChunkPlugin(),
     react({
       babel: {
-        plugins: ['babel-plugin-macros', 'babel-plugin-relay'],
+        plugins: ['babel-plugin-relay'],
       },
     }),
   ],
