d6c1fd81a26c6105f3f305d80e5ac28673e75d17
Daniel Woelfel <dwwoelfel@gmail.com>
2023-06-05 22:56:36 -0700

    remove client setup

--- latch-server/src/index.ts
+++ latch-server/src/index.ts
@@ -1,6 +1,5 @@
 import {createServer} from './server/server.js';
 import process from 'process';
-import {LatchClient, LatchFlagConfig} from 'latch-node-sdk';
 import {initializeConfig} from './config.js';
 
 async function start() {
@@ -11,27 +10,6 @@ async function start() {
     config,
   });
 
-  const latchClient = new LatchClient<{
-    caitlin_mood: LatchFlagConfig<'string'>;
-  }>({
-    flags: {
-      caitlin_mood: {defaultValue: 'happy', flagType: 'string'},
-    },
-    bucketName: config.bucketName,
-    projectId: config.projectId,
-    topicName: config.topicName,
-    environment: config.env,
-  });
-  latchClient.on('flagUpdated', ({key, value, previousValue}) => {
-    console.log('%s updated from %s to %s', key, previousValue, value);
-  });
-  latchClient.on('error', (err) => {
-    console.error('Latch error', err);
-  });
-  console.time('latchClient.init');
-  await latchClient.init();
-  console.timeEnd('latchClient.init');
-
   server.listen({
     port,
     host: config.env === 'development' ? 'localhost' : '0.0.0.0',
@@ -41,11 +19,7 @@ async function start() {
 
   const shutdownImpl = async () => {
     console.log('got shutdown');
-    await Promise.all([
-      server.close(),
-      shutdownServer(),
-      latchClient.shutdown(),
-    ]);
+    await Promise.all([server.close(), shutdownServer()]);
     process.exit(1);
   };
 
@@ -56,8 +30,6 @@ async function start() {
     shutdownPromise = shutdownImpl();
     await shutdownPromise;
   };
-
-  // const init = client.init();
 
   process.on('SIGTERM', shutdown);
   process.on('SIGINT', shutdown);
